2488305006f732bd5cc61dd9266da2d2f61ddaa2
julesAudureau <47111101+julesAudureau@users.noreply.github.com>
2019-03-12 21:39:24 +0100

    Create places.c

---
+++ places.c
@@ -0,0 +1,30 @@
+//ci-dessous l'application places qui va contabilisé le nombres de places disponibles pour l'application concert.
+
+
+
+int main (){
+// definire nombres de places initiales
+
+// connexion à concert
+
+// while 1
+
+//attend information transactions
+
+// si transactions>0
+
+// places->categories =+ transactions
+
+//envoie transactions à concert
+
+//sinon pour i de 0 à transaction
+
+//places->categories =- 1
+// if places == 0 envoie transactions+i break?
+
+//fin si 
+//fin pour 
+//fin while
+
+return 0;
+}
