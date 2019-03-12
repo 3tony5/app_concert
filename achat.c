f972618846ce371f328d2c9b8f9ced7f5e111ec7
julesAudureau <47111101+julesAudureau@users.noreply.github.com>
2019-03-12 21:38:26 +0100

    Create achat.c

---
+++ achat.c
@@ -0,0 +1,29 @@
+// ci dessous l'application achat qui va contenir les clients de l'application concert
+int main(){
+// etablir la connexion avec concert
+
+//tant que transaction non terminés.
+
+//demander un certain nombre de places pour une certaines categories
+
+// envoyer ces données a concert
+
+// récuperer le nombres de places autoriser
+
+//si nbautorisé = nbdemandé
+
+//demandé et envoyer code carte bleu
+
+// attendre ok de concert
+
+// fin de la transaction
+
+// Fin si sinon si nbautorisé =0 fin de la transaction
+
+// sinon présenté offres 
+
+// fin du tant que 
+
+
+return 0;
+}
